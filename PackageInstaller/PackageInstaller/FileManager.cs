47c3340c9f2a5741092a57da626c8b36c292ff18
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-08-24 11:18:45 +0300

    package installing & desktop shortcut should work

--- PackageInstaller/PackageInstaller/FileManager.cs
+++ PackageInstaller/PackageInstaller/FileManager.cs
@@ -8,16 +8,17 @@ using System.Reflection;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
-
+using IWshRuntimeLibrary;
 namespace PackageInstaller
 {
     public class FileManager
     {
         string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-        string desktopfile = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test.zip";
-        float version = 0.65f;
+        string tempfile = Path.GetTempPath() + "\\temp.zip";
+        float version = 0.1f;
         static string ProductName = "Testi";
         string ProgramFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) + "\\NiiloPoutanen\\" + ProductName;
+        string Exename = "Tetris.exe";
 
         public void ChooseFolder()
         {
@@ -31,7 +32,7 @@ namespace PackageInstaller
         {
 
             Stream stream = new MemoryStream(Properties.Resources.Package);
-            FileStream fileStream = new FileStream(desktopfile, FileMode.Create);
+            FileStream fileStream = new FileStream(tempfile, FileMode.Create);
             for (int i = 0; i < stream.Length; i++)
                 fileStream.WriteByte((byte)stream.ReadByte());
             fileStream.Close();
@@ -45,7 +46,16 @@ namespace PackageInstaller
         }
         public bool CompareVersion(float newversion)
         {
-            var oldversionstring = File.ReadAllLines(ProgramFiles + "\\version.txt");
+            string[] oldversionstring;
+            try
+            {
+                oldversionstring = System.IO.File.ReadAllLines(ProgramFiles + "\\version.txt");
+            }
+            catch (Exception)
+            {
+                return true;
+            }
+
             float oldversion = Convert.ToSingle(oldversionstring[0]);
 
             if(oldversion == newversion)
@@ -58,18 +68,41 @@ namespace PackageInstaller
             }
             else if (newversion < oldversion)
             {
-                MessageBox.Show("You are trying to install a older version than the version currently installed.");
+                DialogResult result = MessageBox.Show("You are trying to install a older version than the version currently installed. Click YES if you wish to continue.","Version issue",MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    return true;
+                }
             }
             return false;
         }
         public void ClearDirectory()
         {
             DirectoryInfo drinfo = new DirectoryInfo(ProgramFiles);
+            DirectoryInfo[] folders = drinfo.GetDirectories();
             FileInfo[] files = drinfo.GetFiles();
             foreach (FileInfo file in files)
             {
                 file.Delete();
             }
+            foreach(DirectoryInfo folder in folders)
+            {
+                folder.Delete(true);
+            }
+        }
+        public void CreateShortcut()
+        {
+            string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string desktopShortcut = Path.Combine(desktop, ProductName + ".lnk");
+            WshShell shell = new WshShell();
+            IWshShortcut link = (IWshShortcut)shell.CreateShortcut(desktopShortcut);
+            link.TargetPath = ProgramFiles + "\\" +Exename;
+            link.Save();
+            DeleteTemp();
+        }
+        public void DeleteTemp()
+        {
+            System.IO.File.Delete(tempfile);
         }
         public void ExtractZip()
         {
@@ -82,20 +115,19 @@ namespace PackageInstaller
                 }
                 else if (!Directory.EnumerateFileSystemEntries(ProgramFiles).Any())
                 {
-                    ZipFile.ExtractToDirectory(desktopfile, ProgramFiles);
+                    ZipFile.ExtractToDirectory(tempfile, ProgramFiles);
                     using (StreamWriter versionwriter = new StreamWriter(ProgramFiles + "\\version.txt"))
                     {
                         versionwriter.WriteLine(version);
                     }
+                    CreateShortcut();
 
                 }
             }
             else if (!(Directory.Exists(ProgramFiles)))
             {
-                ZipFile.ExtractToDirectory(desktopfile, ProgramFiles);
+                ZipFile.ExtractToDirectory(tempfile, ProgramFiles);
             }
-
-
         }
     }
 }
