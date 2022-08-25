d3539fc693d24bfef279c153fbf2ac7a2bcc0274
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-08-25 17:49:50 +0300

    fixed resources

--- WPF/WPFinstaller/WPFinstaller/MainWindow.xaml.cs
+++ WPF/WPFinstaller/WPFinstaller/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +15,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFinstaller.Properties;
+using System.Drawing;
 
 namespace WPFinstaller
 {
@@ -24,6 +28,7 @@ namespace WPFinstaller
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         private void Form_MouseDown(object sender, MouseButtonEventArgs e)
