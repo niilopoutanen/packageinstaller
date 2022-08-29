08de82a7c7e2d9180078de7efc73cbe3c7131ced
Niilo Poutanen <niilo.poutanen@gmail.com>
2022-08-29 09:57:08 +0300

    design 2

---
+++ WPF/Design2/WPFinstaller/WPFinstaller/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFinstaller
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InstallDonePanel.Visibility = Visibility.Hidden;
+            InstallDonePanel.Margin = new Thickness(0, 0, 0, 0);
+        }
+
+
+        private void InstallApp()
+        {
+            InstallPanel.Visibility = Visibility.Hidden;
+            InstallDonePanel.Visibility = Visibility.Visible;
+        }
+
+
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+        private void CloseApp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void MinimizeApp(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void OpenLink(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://github.com/niilopoutanen") { UseShellExecute = true });
+        }
+        private void InstallButton_Click(object sender, MouseButtonEventArgs e)
+        {
+            InstallApp();
+        }
+    }
+}
